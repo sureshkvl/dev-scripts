ebd0a28a2dda6887890168cb4eba57ba9a651850
Your Name <you@example.com>
2017-09-16 07:45:45 +0000

    added

--- perf-tests/mgen_runner.py
+++ perf-tests/mgen_runner.py
@@ -4,6 +4,7 @@ import argparse
 from threading import Timer
 from time import sleep
 import time
+import subprocess
 """
 python mgen-client.py -- server-ip 10.0.1.3 --server-port 5000  --duration 600 --pkt-size 64 \
       --connections 1000  --connections-step 1000
@@ -16,7 +17,7 @@ oname = "outputtx.log"
 def process_mgen_output():
     pass
 
-def run_mgen():
+def run_mgen_test():
     #mgen input input.mgen txlog output outputtx.log
     retcode = subprocess.call(["mgen", "input", fname, "txlog", "output", oname])
     print retcode
@@ -29,21 +30,26 @@ def writefile(filename, data):
     file.close()
 
 def write_mgen_file(args):
+    # clean up the exisitng files
+    os.remove(fname)
+    os.remove(oname)
     # 0.0 ON 1 UDP DST 10.24.0.3/5001 PERIODIC [1000 1240]
     # 60.0OFF 1
-    for i in range(1, int(args.flows)):
-        d = "0.0 ON "+ str(i) + " UDP DST " + str(args.server_ip) + "/" + str(args.server_port) + " PERIODIC ["+ str(args.pkt_size)+ " "+ str(args.pkts_per_sec)+ "] " 
+    flowno = 1
+    for i in range(0, int(args.flows)):
+        d = "0.0 ON "+ str(flowno) + " UDP DST " + str(args.server_ip) + "/" + str(args.server_port) + " PERIODIC ["+ str(args.pkts_per_sec)+ " "+ str(args.pkt_size)+ "] " 
         writefile(fname,d)
-    flowno = i
+        flowno = flowno + 1
+    
     if args.flows_increase_step:
         for i in range(1, int(args.duration)):
-            for f in range(1, int(args.flows_increase_step)):
-                flowno = flowno + 1
-                d = str(i) + ".0 ON "+ str(flowno) + " UDP DST " + str(args.server_ip) + "/" + str(args.server_port) + " PERIODIC ["+ str(args.pkt_size)+ " "+ str(args.pkts_per_sec)+ "] " 
+            for f in range(0, int(args.flows_increase_step)):
+                d = str(i) + ".0 ON "+ str(flowno) + " UDP DST " + str(args.server_ip) + "/" + str(args.server_port) + " PERIODIC ["+ str(args.pkts_per_sec) + " "+ str(args.pkt_size)+ "] " 
                 writefile(fname,d)
+                flowno = flowno + 1
 
     for i in range(1, flowno):
-        d =  str(args.duration) + ".0 0FF " + str(i)
+        d =  str(args.duration) + ".0 OFF " + str(i)
         writefile(fname, d)
 
 def main(argv):
